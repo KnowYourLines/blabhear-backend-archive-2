3bd85f26dd2c37ed7e95cdc2571dc07bd6f3af1e
Johnny <knowyourlines@outlook.com>
2023-02-17 16:32:06 +0000

    added distortion

--- api/constants.py
+++ api/constants.py
@@ -2,12 +2,13 @@ import os
 
 from deepgram import Deepgram
 from pedalboard import Pedalboard
-from pedalboard_native import PitchShift
+from pedalboard_native import PitchShift, Distortion
 
 DEEPGRAM_CLIENT = Deepgram(os.environ.get("DEEPGRAM_API_KEY"))
 VOICE_EFFECTS = {
     "High Pitch": Pedalboard([PitchShift(5)]),
     "Low Pitch": Pedalboard([PitchShift(-5)]),
+    "Fuzzy": Pedalboard([Distortion()]),
 }
 LANGUAGES = [
     ("en", "English"),
