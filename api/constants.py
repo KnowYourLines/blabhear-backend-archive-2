bf62ef8bc1f218399a66d0290e5b7a7acc66b3b2
Johnny <knowyourlines@outlook.com>
2023-02-17 16:57:35 +0000

    added echo

--- api/constants.py
+++ api/constants.py
@@ -2,13 +2,14 @@ import os
 
 from deepgram import Deepgram
 from pedalboard import Pedalboard
-from pedalboard_native import PitchShift, Chorus
+from pedalboard_native import PitchShift, Chorus, Delay
 
 DEEPGRAM_CLIENT = Deepgram(os.environ.get("DEEPGRAM_API_KEY"))
 VOICE_EFFECTS = {
     "High Pitch": Pedalboard([PitchShift(5)]),
     "Low Pitch": Pedalboard([PitchShift(-5)]),
     "Wobbly": Pedalboard([Chorus(rate_hz=10, mix=1)]),
+    "Echo": Pedalboard([Delay(delay_seconds=0.125, feedback=0.75, mix=0.5)]),
 }
 LANGUAGES = [
     ("en", "English"),
