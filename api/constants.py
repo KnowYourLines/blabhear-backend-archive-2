b7f593c60d34d7b69a7eff2fddf42c1d4e640f36
Johnny <knowyourlines@outlook.com>
2023-02-17 20:00:28 +0000

    fine tune effects and add copycats effect

--- api/constants.py
+++ api/constants.py
@@ -2,18 +2,66 @@ import os
 
 from deepgram import Deepgram
 from pedalboard import Pedalboard
-from pedalboard_native import PitchShift, Chorus, Delay
+from pedalboard_native import PitchShift, Chorus, Delay, Reverb, Gain
+from pedalboard_native.utils import Mix
 
 DEEPGRAM_CLIENT = Deepgram(os.environ.get("DEEPGRAM_API_KEY"))
 VOICE_EFFECTS = {
     "High Pitch": Pedalboard([PitchShift(5)]),
     "Low Pitch": Pedalboard([PitchShift(-5)]),
-    "Wobble": Pedalboard([Chorus(rate_hz=10, mix=1)]),
-    "Echo": Pedalboard([Delay(delay_seconds=0.125, feedback=0.75, mix=0.5)]),
-    "High Echo": Pedalboard([PitchShift(5), Delay(delay_seconds=0.125, feedback=0.75, mix=0.5)]),
-    "Low Echo": Pedalboard([PitchShift(-5), Delay(delay_seconds=0.125, feedback=0.75, mix=1)]),
-    "High Wobble": Pedalboard([PitchShift(5), Chorus(rate_hz=10, mix=1)]),
-    "Low Wobble": Pedalboard([PitchShift(-5), Chorus(rate_hz=10, mix=1)]),
+    "Wobble": Pedalboard([Chorus(rate_hz=10, mix=0.75)]),
+    "Echo": Pedalboard(
+        [Delay(delay_seconds=0.125, feedback=0.75, mix=0.5), Gain(gain_db=3)]
+    ),
+    "High Echo": Pedalboard(
+        [
+            PitchShift(5),
+            Delay(delay_seconds=0.125, feedback=0.75, mix=0.5),
+            Gain(gain_db=3),
+        ]
+    ),
+    "Low Echo": Pedalboard(
+        [
+            PitchShift(-5),
+            Delay(delay_seconds=0.125, feedback=0.75, mix=1),
+            Gain(gain_db=3),
+        ]
+    ),
+    "High Wobble": Pedalboard(
+        [PitchShift(5), Chorus(rate_hz=10, mix=0.75), Gain(gain_db=3)]
+    ),
+    "Low Wobble": Pedalboard(
+        [PitchShift(-5), Chorus(rate_hz=10, mix=0.75), Gain(gain_db=3)]
+    ),
+    "Copycats": Pedalboard(
+        [
+            Mix(
+                [
+                    Pedalboard(
+                        [
+                            PitchShift(-5),
+                        ]
+                    ),
+                    Pedalboard(
+                        [
+                            PitchShift(-3),
+                            Delay(delay_seconds=0.5, mix=1.0),
+                            Gain(gain_db=3),
+                        ]
+                    ),
+                    Pedalboard(
+                        [
+                            PitchShift(3),
+                            Delay(delay_seconds=0.25, mix=1.0),
+                            Gain(gain_db=3),
+                        ]
+                    ),
+                    Pedalboard([PitchShift(5)]),
+                ]
+            ),
+            Reverb(),
+        ]
+    ),
 }
 LANGUAGES = [
     ("en", "English"),
